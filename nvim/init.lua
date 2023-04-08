1eb78c17e298408d52384612de70db1d51b78d04
vonhyou <moi@rfung.dev>
2023-04-07 23:40:30 -0300

    add neovim config folder

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+require("settings")
+require("plugins")
+
+vim.cmd.colorscheme "catppuccin-latte"
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,21 @@
+local global = vim.g
+local o = vim.o
+
+vim.scriptencoding = "utf-8"
+
+-- Map <leader>
+
+global.mapleader = " "
+global.maplocalleader = " "
+
+-- Editor options
+
+o.number = true
+o.relativenumber = true
+o.autoindent = true
+o.syntax = "on"
+o.autoindent = true
+o.cursorline = true
+o.shiftwidth = 2
+o.tabstop = 2
+o.encoding = "utf-8"
